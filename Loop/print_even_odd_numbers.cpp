c5fe56fe985caf819512a952fc1190c3f2b4229f
Rahul Kumar Bhuyan <bhuyanrahul2006@gmail.com>
2026-03-02 17:04:53 +0530

    Update print_even_odd_numbers.cpp

--- Loop/print_even_odd_numbers.cpp
+++ Loop/print_even_odd_numbers.cpp
@@ -1,3 +1,22 @@
+/*
+========================================================
+Author      : Rahul Kumar Bhuyan
+Repository  : DSA-Practice
+Folder Name : Loop
+File Name   : print_even_odd_numbers.cpp
+Topic       : Loop (Basic Iteration)
+Problem     : Print even and odd numbers from 1 to n number
+Language    : C++
+========================================================
+Time Complexity  : O(n)
+- Loop runs 'n' times 
+- Each iteration does constant work
+
+Space Complexity : O(1)
+- No extra data structures used
+- Only few integer variables
+*/
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -16,3 +35,4 @@ int main(){
 	return 0;
 	
 }
+
