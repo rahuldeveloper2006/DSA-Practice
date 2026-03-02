be0378eabaeb0639a3a9ab262da2882e44c0cbeb
Rahul Kumar Bhuyan <bhuyanrahul2006@gmail.com>
2026-03-02 16:22:04 +0530

    added : print multiplication table of numbers

---
+++ Loop/print_table_of_num.cpp
@@ -0,0 +1,32 @@
+/*
+========================================================
+Author      : Rahul Bhuyan
+Repository  : DSA Practice (C++)
+Folder Name : Loop
+File Name   : print_table.cpp
+Topic       : Loop (Basic Iteration)
+Problem     : Print multiplication table of a number
+Language    : C++
+Compiler    : g++
+========================================================
+Time Complexity  : O(n)
+- Loop runs 'n' times (here n = 10)
+- Each iteration does constant work
+
+Space Complexity : O(1)
+- No extra data structures used
+- Only few integer variables
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+	int n,i=1;
+	cout<<"Enter number : ";
+	cin>>n;
+	while(i<=10){
+		cout<<n<<"*"<<i<<"="<<n*i;
+		cout<<endl;
+		i++;
+	}
+}
